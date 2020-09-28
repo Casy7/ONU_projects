f2028160123bb50babd4d204dec0fea8df234d28
Casy7 <kasjanov.jaroslav@rl.odessa.ua>
2020-09-28 23:26:44 +0300

    lab1 C#

--- C#/Lab1/Program.cs
+++ C#/Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Math;
 
 namespace Lab1
 {
@@ -6,7 +7,30 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // This code outputs the solution to this equation.
+            //                      x^2.8                               x
+            //      y= —————————————————————————————————  - arctg(——————————————)^5
+            //         ( cos( x^3 - 3.7 )^4 + |3x|^0.5 )             lg(|x|)
+            // 
+
+            Console.WriteLine("Введите x для вычисления значения y=f(x): ");
+
+            string string_x = Console.ReadLine().Replace(".", ",");
+            double x = Convert.ToDouble(string_x);
+
+            double y =                      Pow(x, 2.8) /
+            //         ——————————————————————————————————————————————————
+                       (Pow(Cos(Pow(x, 3) - 3.7), 4) + Pow(Abs(3 * x), 0.5))
+
+                       -
+
+                       Pow(Atan(x / Log10(Abs(x))), 5)
+            ;
+            y = Round(y, 4);
+
+
+            Console.WriteLine($"Выходное значение y: {y}");
+            Console.ReadLine();
         }
     }
 }
